9927188783e0983ac9ae5d193d066e5c7d40bc8e
chiefautoparts <chiefautoparts@outlook.com>
2018-05-27 09:54:23 -0700

    email backend

--- homePage/views.py
+++ homePage/views.py
@@ -1,9 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from django.template.loader import get_template
 
 # Create your views here.
 def index(request):
 	return render(request, 'homePage/home.html')
 
+def contact(request):
+	form_class = ContactForm
+
+	if request.method == 'POST':
+		form = form_class(data=request.POST)
+
+		if form.is_valid():
+			contact_name = request.POST.get(
+				'contact_name'
+				, '')
+			contact_email = request.POST.get(
+
+				'contact_email',
+				'')
+			form_content = request.POST.get('content', '')
+
+			template = get_template('contact_template.txt')
+			context = {
+				'contact_name': contact_name,
+				'contact_email': contact_email,
+				'form_content': form_content,
+			}
+			content = template.render(context)
+
+			email =EmailMessage(
+				"New contact form submission", content, "Your website " + '', ['chiefautoparts@outlook.com'], headers = {'Reply-To': contact_email })
+			email.send()
+			return redirect('contact')
+
+	return render(request, 'homePage/contact.html', {'form': form_class, })
--- landscapeCo/settings.py
+++ landscapeCo/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# EMAIL BACKEND SETUP
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'chiefautoparts@outlook.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
